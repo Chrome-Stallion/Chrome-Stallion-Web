ec0c1ff4aa8699a31332883590e4826cd52c742f
Tam Mendera <mendera.1@osu.edu>
2022-03-04 22:34:22 -0500

    Catalog Feature

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,9 +1,25 @@
-function Products() {
-    return (
-        <div>
-            <h2>Products</h2>
-        </div>
-    );
-}
+import React from 'react';
+import data from './data';
 
-export default Products;
+<div className = "content">
+    <ul className = "products">
+        {
+            // missing an arrow below => I didnt know how to add the arrow
+            data.products.map(product 
+                <li>
+                    <div className ="Product">
+                        <img className="product-image" src={product.imageUrl} alt="product"/>
+                        <div className="product-name">
+                            <a href="product.html">{product.name}</a>
+
+                         </div>
+                        <div className="product-brand">{product.brand}</div>
+                         <div className="product-price">{product.price}</div>
+                         <div className="product-rating">{product.rating} Stars ({product.numberOfReviews} Reviews)</div>
+                    </div>
+                </li> )
+        }
+    
+     </ul>
+</div>
+    
