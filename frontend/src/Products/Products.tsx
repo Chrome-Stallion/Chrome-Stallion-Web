15c011bb6c09400a4df121395125ac11e6241062
Ben Kendall <Benjaminryankendall@gmail.com>
2022-03-06 15:25:15 -0500

    Through Page 32: Persisting error with Products class tag in App.tsx

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import data from './data';
 
-<div className = "content">
+//I think what I commented out here can be deleted
+//<div className = "content">
     <ul className = "products">
         {
-            // missing an arrow below => I didnt know how to add the arrow
-            data.products.map(product 
+            data.products.map((product)=>(
                 <li>
-                    <div className ="Product">
-                        <img className="product-image" src={product.imageUrl} alt="product"/>
+                    <div className ="product">
+                        <img 
+                            className="product-image" 
+                            src={product.imageUrl} 
+                            alt="product"
+                        />
                         <div className="product-name">
                             <a href="product.html">{product.name}</a>
-
-                         </div>
+                        </div>
                         <div className="product-brand">{product.brand}</div>
-                         <div className="product-price">{product.price}</div>
-                         <div className="product-rating">{product.rating} Stars ({product.numberOfReviews} Reviews)</div>
+                        <div className="product-price">{product.price}</div>
+                        <div className="product-rating">
+                            {product.rating} Stars ({product.numberOfReviews} Reviews)
+                        </div>
                     </div>
-                </li> )
+                </li> 
+            ))
         }
-    
-     </ul>
-</div>
+        ;
+    </ul>
+//</div>
     
