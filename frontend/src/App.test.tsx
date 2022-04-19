f37845e8ff6643f38d65fb868f42a5b5380ca621
John Browne <brownej1331@gmail.com>
2022-04-19 17:31:08 -0400

    Lab 5 Part 2.2, App Tests - Advanced

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -7,12 +7,23 @@ describe('The main app', () => {
 
   it('should render the Orders link', () => {
     render(<App />);
-    expect(screen.getByText('Orders')).toBeInTheDocuments();
+    expect(screen.getByText('Orders')).toBeInTheDocument();
   });
 
   it('payments component should be in app component', () => {
     render(<App />);
     expect(screen.getByText('Payment')).toBeInTheDocument();
+  });
+
+  it('should render the Home link', () => {
+    render(<App />);
+    // expect(screen.getByText('Home')).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: "Home" })).toBeInTheDocument();
+  });
+
+  it('should render the Home header', () => {
+    render(<App />);
+    expect(screen.getByRole('heading', { level: 2 })).toBeInTheDocument();
   });
 
 });
